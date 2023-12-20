72d4b80fd15f228206ae345bcd31ab6f1d0d1d2e
YasmineRehman <chacharmeenu@gmail.com>
2023-12-20 17:37:34 +0500

    my commit

--- src/components/Herosection.js
+++ src/components/Herosection.js
@@ -1,35 +1,57 @@
-import React from "react";
-import mockup from "../assets/img/Mockup.png";
-import { Image } from "react-bootstrap";
-import play from "../assets/img/Play 02.png";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-export default function HeroSection() {
+import { faPlay } from '@fortawesome/free-solid-svg-icons';
+import Image from '../assets/images/Mockup.png';
+
+const Herosection = () => {
+  const style = {
+    background: 'linear-gradient(to right, rgba(24, 173, 228, 1), rgba(239, 143, 45, 1), rgba(170, 88, 170, 1))',
+    // Add other styles as needed
+  };
+
+  const circleStyle = {
+    borderRadius: '60%',
+    backgroundColor: 'red', // Set the background color to red
+    padding: '20px',
+    display: 'inline-block',
+  };
+
   return (
-    <div div style={{ backgroundColor: "black" }}>
-      <div className="herosection">
-        <div>
-          <h1>
-            Web3Makr Redefining <br />
-            How You Create
-          </h1>
-          <p>
-            Unlock the blockchain potential to create <br /> Web3 Applications
+    <Container fluid style={style}>
+      <Row className="d-flex align-items-center">
+        <Col md={12} className="text-center">
+          <p style={{ fontFamily: 'Inter', color: 'white', fontSize: '5vw' ,fontWeight: 'bold' }}>
+            Web3Makr Redeffining
           </p>
-          <Link to={"/"}>
-            <Image src={play} style={{ marginTop: "1rem" }} />
-          </Link>
-        </div>
-      </div>
-      <div
-        style={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <Image src={mockup} width={"900px"} />
-      </div>
-    </div>
+        </Col>
+        <Col md={12} className="text-center">
+          <p style={{ fontFamily: 'Inter', color: 'white', fontSize: '5vw',fontWeight: 'bold'  }}>
+            How You Create
+          </p>
+        </Col>
+        <Col md={12} className="text-center">
+          <p style={{ fontFamily: 'Inter', color: 'white', fontSize: '2vw' }}>
+            Unlock the blockchain potential to create
+          </p>
+        </Col>
+        <Col md={12} className="text-center">
+          <p style={{ fontFamily: 'Inter', color: 'white', fontSize: '2vw' }}>
+            Web3 Applications
+          </p>
+        </Col>
+        <Col md={12} className="text-center">
+          <div style={circleStyle}>
+            <FontAwesomeIcon icon={faPlay} style={{ color: 'white', fontSize: '4vw' }} />
+          </div>
+        </Col>
+        <Col md={12} className="text-center">
+          <img src={Image} alt='' className="img-fluid" />
+        </Col>
+      </Row>
+    </Container>
   );
 }
+
+export default Herosection;
